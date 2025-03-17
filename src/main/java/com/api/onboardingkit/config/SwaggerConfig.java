51e674ad9c72d84dd5d03287bcf13be9055aaf94
1117mg <lee_yerim@naver.com>
2025-03-17 14:34:37 +0900

    feat: swagger api 문서

---
+++ src/main/java/com/api/onboardingkit/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.api.onboardingkit.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("신입키트 API 문서")
+                .description("Onboarding-kit의 Swagger API 문서입니다.")
+                .version("1.0.0");
+    }
+}
